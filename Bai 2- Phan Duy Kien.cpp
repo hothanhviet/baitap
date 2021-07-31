ad63ae6330550b5efe39476f8e8fb0c6de3305c3
hothanhviet <88219245+hothanhviet@users.noreply.github.com>
2021-07-31 11:54:37 +0700

    Update Bai 2- Phan Duy Kien.cpp
    Sửa lại bài

--- Bai 2- Phan Duy Kien.cpp
+++ Bai 2- Phan Duy Kien.cpp
@@ -1,15 +1,18 @@
 #include<stdio.h>
 
 int main() {
-	int n;
-	scanf("%d",&n);
-	for ( int i=2; i<= (n-1); i++)
-	{ 
-		if ( n % i ==0) 
-		{
-			printf("%d khong la so nguyen to",n); break;
+if ( n == 2 ) { 
+		printf("%d la so nguyen to",2);
 		}
-	printf("%d la so nguyen to",n); break;
+else {
+	for ( i=2; i<= (n-1); i++) { 
+		if ( n % i ==0)
+			{
+			printf("%d khong la so nguyen to",n); break;
+			}
+		printf("%d la so nguyen to",n); break;
+		}	
+		}
 	}
 	return 0;
 }
